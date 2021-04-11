72aa4f7ff8b6d451964410983e8fd72512f0a477
freeyoung <lavave@gmail.com>
2021-04-12 00:29:48 +0200

    revert to old label to not break existing migrations

--- alipay/create_direct_pay_by_user/dpn/apps.py
+++ alipay/create_direct_pay_by_user/dpn/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AlipayDPNConfig(AppConfig):
     name = 'alipay.create_direct_pay_by_user.dpn'
-    label = 'alipay_direct_pay'
+    label = 'dpn'
--- alipay/create_partner_trade_by_buyer/ptn/apps.py
+++ alipay/create_partner_trade_by_buyer/ptn/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class AlipayPTNConfig(AppConfig):
     name = 'alipay.create_partner_trade_by_buyer.ptn'
-    label = 'alipay_partner_trade'
+    label = 'ptn'
